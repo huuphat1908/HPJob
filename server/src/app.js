0093c3416dd85fe0cee3da5743f7e94afcfbf685
huuphat1908 <huuphat1908@gmail.com>
2021-10-17 12:13:58 +0700

    add and config public folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,8 @@
 //import dependency
 import express from 'express';
 import dotenv from 'dotenv';
-/* import path from 'path'; */
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 
@@ -12,11 +13,12 @@ import * as db from './config/db/index.js';
 //app port req.body router db static files
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 route(app);
 db.connect();
-/* app.use(express.static(path.join(__dirname, 'public'))); */
 
 app.listen(PORT, () =>
     console.log(`App listening on port ${PORT}`)
