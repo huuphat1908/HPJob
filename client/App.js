652c207ecd5626c7e54f93f137bb7884bfbe8201
huuphat1908 <huuphat1908@gmail.com>
2021-12-10 00:47:36 +0700

    restart client folder

--- client/App.js
+++ client/App.js
@@ -1,9 +1,36 @@
-import React from 'react'; 
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
-import Note from './components/Note';
+const SampleStyledComponent = styled.View`
+  font-size: 50px;
+  background-color: red;
+`;
+
+const Another = styled.Text`
+  color: red;
+`;
 
 export default function App() {
   return (
-    <Note title='Note Title' text='Note Text'/>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <SampleStyledComponent>
+        <Text>Arsenal</Text>
+      </SampleStyledComponent>
+      <Another>MU</Another>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- client/configs/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://hp-note.herokuapp.com/';
-
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-export default axios;
