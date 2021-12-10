973d6f8077c896952ff316eca12b66c5a2cc5c25
huuphat1908 <huuphat1908@gmail.com>
2021-12-11 05:09:20 +0700

    install package for client folder

--- client/App.js
+++ client/App.js
@@ -1,30 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const SampleStyledComponent = styled.View`
-  font-size: 50px;
-  background-color: red;
-`;
-
-const Another = styled.Text`
-  color: red;
-`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <SampleStyledComponent>
-        <Text>Arsenal</Text>
-      </SampleStyledComponent>
-      <Another>MU</Another>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
